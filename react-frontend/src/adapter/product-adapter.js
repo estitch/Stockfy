b7f401d81f3791586489955a3d963b3480addf37
ludemo <azevallosa@unsa.edu.pe>
2024-11-26 22:03:33 -0500

    chore:Ordenamiento de productos por id descendente

--- react-frontend/src/adapter/product-adapter.js
+++ react-frontend/src/adapter/product-adapter.js
@@ -3,7 +3,7 @@ import { productService } from '../services/product-service'
 class ProductAdapter {
   transformedProductData(product) {
     return {
-      id: product.dynamo,
+      id: Number(product.dynamo),
       code: product.Code,
       name: product.Name,
       description: product.Description,
@@ -15,31 +15,30 @@ class ProductAdapter {
 
   async getTransformedProducts() {
     try {
-      const response = await productService.getProduct(); // Obtiene el JSON
-      //console.log('Response from productService:', response); // Inspección
-
+      const response = await productService.getProduct();
+  
       const parsedBody = JSON.parse(response.body);
-      //console.log(parsedBody); // Asegúrate de que esto contiene los datos esperados
       const productsData = parsedBody.data;
-      //console.log(productsData); // Verifica que contenga la lista de productos
-
-      // Extraer los datos del JSON
-      const products = productsData || []; // Cambia `data` según la clave del JSON
-      //console.log(products)
-
-      // Verificar que sea un array antes de aplicar map
+  
+      const products = productsData || [];
+  
       if (!Array.isArray(products)) {
         throw new Error('La respuesta no contiene un array de productos');
       }
-
-      // Transformar los productos
-      return products.map(product => this.transformedProductData(product));
+  
+      // Transformar los productos y verificar valores
+      const transformedProducts = products
+        .map(product => {
+          return this.transformedProductData(product);
+        })
+        .filter(Boolean); // Eliminar productos nulos
+      return transformedProducts.sort((a, b) => b.id - a.id)
     } catch (error) {
       console.error('Error al obtener productos:', error);
       return [];
     }
   }
-
+  
   async createProduct(productData) {
     await productService.createProduct(productData);
   }
